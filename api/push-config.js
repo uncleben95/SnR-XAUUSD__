ba68bdacc13d172047ee6edb6fbfc465a8b9ae04
uncleben95 <uncleben95@gmail.com>
2026-09-21 13:12:25 +0800

    Delete api/push-config.js

--- api/push-config.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Cache-Control", "no-store");
-
-  return res.status(200).json({
-    publicKey: process.env.VAPID_PUBLIC_KEY || null
-  });
-}
